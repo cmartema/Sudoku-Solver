a0c77b25424b48e1b6adbd28cf3c5079afa73b73
cmartema <cjm2301@columbia.edu>
2023-10-15 02:55:56 -0400

    Update

--- sudoku.py
+++ sudoku.py
@@ -31,72 +31,81 @@ def board_to_string(board):
     return ''.join(ordered_vals)
 
 def is_complete(board):
-    """Checks to see if there are any values removed."""
-    for row in board:
-        if 0 in row:
-            return False  # There is at least one unassigned variable in rows
+    """Checks if the Sudoku board is complete."""
+    # Check if all cells are assigned
+    for var in board:
+        if board[var] == 0:
+            return False  # There is at least one unassigned variable
 
-    for col in range(9):
-        if any(board[row][col] == 0 for row in range(9)):
-            return False  # There is at least one unassigned variable in columns
-
-    # Check 3x3 boxes
-    for box_row in range(0, 9, 3):
-        for box_col in range(0, 9, 3):
-            if any(board[row][col] == 0 for row in range(box_row, box_row + 3) for col in range(box_col, box_col + 3)):
-                return False  # There is at least one unassigned variable in a 3x3 box
+    # Check if the board satisfies Sudoku constraints (no repeated values in rows, columns, and 3x3 boxes)
+    for row in ROW:
+        for col in COL:
+            value = board[row + col]
+            if not is_consistent(value, row + col, board):
+                return False
     return True
-    
+
 def  select_unassigned_var(board):
-    """Chooses the unassigned variable with the smallest domain size (MRV)."""
-    var = None
-    min_domain = float('inf') #Sets it to infinity 
-    for row in range(9):
-        for col in range(9):
-            if board[row][col] == 0:
-                if domain_size((row, col), board) < min_domain:
-                    var = (row, col)
-                    min_domain = domain_size(var, board)
-    return var
+    """"Selects an unassigned variable using the minimum remaining value heuristic."""
+    unassigned_vars = []
+    for var in board:
+        if board[var] == 0:
+            unassigned_vars.append(var)
+
+        #remaining values for each unassigned variable
+        remaining_values = {}
+        for var in unassigned_vars:
+            remaining_values[var] = 0
+            for value in range(1, 10):
+                if is_consistent(value, var, board):
+                    remaining_values[var] += 1
+        #select the unassigned variable with the smallest number of remaining values.
+        min_val = min(remaining_values.values())
+        selected_var = None
+        for var in unassigned_vars:
+            if remaining_values[var] == min_val:
+                selected_var = var
+                break
+            return selected_var
 
 
-def ordered_domain_values():
-    pass
-def is_consistent():
-    pass
+def ordered_domain_values(var, board):
+    """Returns a list of the values in the domain of the variable, sorted inascending order."""
+    domain_values = []
+    for value in range(1,10):
+        if is_consistent(value, var, board):
+            domain_values.append(value)
+    domain_values.sort()
+    return domain_values
 
-def domain_size(var, board):
-    """Returns the size of the domain for the variable"""
-    if board[var[0]][var[1]] != 0:
-        return 1
-    else:
-        valid_values = set(range(1, 10)) 
-        row, col = var
-        #Check row
-        for i in range(9):
-            value = board[row][i]
-            if value in valid_values:
-                valid_values.remove(value)
-        #Check column
-        for i in range(9):
-            value = board[i][col]
-            if value in valid_values:
-                valid_values.remove(value)
-        #Check the 3x3 
-        box_row, box_col = (row // 3) * 3, (col // 3) * 3
-        for i in range(box_row, box_row + 3):
-            for j in range(box_col, box_col + 3):
-                value = board[i][j]
-                if value in valid_values:
-                    valid_values.remove(value)
-
-        return len(valid_values)
-
+def is_consistent(value, var, board):
+    #check if the variable value is in the same row
+    for var2 in board:
+        print("Variable:", {var2}, "Value:", {board[var2]}) 
+        if var2 != var and board[var2] == value and var2[0] == var[0]:
+            return False
+        
+    #check if the variable value is in the same column
+    for var2 in board:
+        if var2 != var and board[var2] == value and var2[1] == var[1]:
+            return False
+        
+    #check if the variable value is in the 3x3 grid
+    row = var[0]
+    col = var[1]
+    row_start = ROW.index(row) // 3 * 3
+    col_start = COL.index(col) // 3 * 3
+    for var2 in board:
+        var2_row = var2[0]
+        var2_col = var2[1]
+        var2_row_index = ROW.index(var2_row)
+        var2_col_index = COL.index(var2_col)
+        if var2 != var and board[var2] == value and var2_row_index >= row_start and var2_row_index < row_start + 3 and var2_col_index >= col_start and var2_col_index < col_start + 3:
+            return False
+    return True
 
 def backtracking(board):
     """Takes a board and returns solved board."""
-    # TODO: implement this
-    failure = board
     if is_complete(board):
         solved_board = board
         return solved_board
@@ -104,12 +113,12 @@ def backtracking(board):
     var = select_unassigned_var(board) # Uses minimum remaining value heuristic
     for value in ordered_domain_values(var, board):
         if is_consistent(value, var, board):
-            board[var[0]][var[1]] = value
+            board[var] = value
             solved_board = backtracking(board)
-            if solved_board != failure:
+            if solved_board is not None:
                 return solved_board
-            board[var[0]][var[1]] = 0 # Removes the variable value from board
-    return failure # Returns failure board
+            board[var] = 0 # Removes the variable value from board
+    return None # Returns failure board
 
 
 if __name__ == '__main__':
@@ -120,22 +129,24 @@ if __name__ == '__main__':
         # Parse boards to dict representation, scanning board L to R, Up to Down
         board = { ROW[r] + COL[c]: int(sys.argv[1][9*r+c])
                   for r in range(9) for c in range(9)}       
-        '''
-        solved_board = backtracking(board)
         
+        solved_board = backtracking(board)
+
+        '''
         # Write board to file
         out_filename = 'output.txt'
         outfile = open(out_filename, "w")
         outfile.write(board_to_string(solved_board))
         outfile.write('\n') 
 
-        '''
-        
-        for row in "ABCDEFGHI":
-            for col in "123456789":
+
+        for row in ROW:
+            for col in COL:
                 cell_name = row + col
                 cell_value = board[cell_name]
                 print(f"Value of cell {cell_name}: {cell_value}")
+
+        '''
 
         print_board(board)
 
